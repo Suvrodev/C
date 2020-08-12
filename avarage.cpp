0d476807bd8ecfff9de44b6c34e56696122ef435
Suvrodev Howlader <61430612+Suvrodev@users.noreply.github.com>
2020-08-13 02:09:43 +0600

    Add files via upload

---
+++ avarage.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int Array[100]={1,2,3,4,5,6,7,8,9,10};
+   int i,Total=0;
+   printf("After Computing Element=");
+   for(i=9;i>=0;i--)
+   {
+       Total=Total+Array[i];
+       printf(" %d",Array[i]);
+   }
+    float Avarage=(float)Total/10;
+    printf("\n Average Value=%.1f",Avarage);
+}
